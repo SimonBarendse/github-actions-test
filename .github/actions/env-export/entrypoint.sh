f2b679438ce6ce5df34bc003571672642b0a7b7e
Simon Barendse <simon.barendse@gmail.com>
2020-02-28 15:38:13 +0100

    Use new SecretHub CLI features: `env ls` and `env read`

--- .github/actions/env-export/entrypoint.sh
+++ .github/actions/env-export/entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 set -e
 
-# Get a list of env vars (this is a hack, we need a command for this)
-env_var_names=$(secrethub run -- env | grep "<redacted by SecretHub>" | cut -d '=' -f 1)
+env_var_names=$(secrethub env ls)
 
 for var_name in $env_var_names; do
-	# Read the secret value (this is a hack)
-	secret_value=$(secrethub run --no-masking -- printenv $var_name)
+	secret_value=$(secrethub env read $var_name)
 	echo "::set-env name=$var_name::$secret_value"
 	echo "::add-mask::$secret_value"
 done
